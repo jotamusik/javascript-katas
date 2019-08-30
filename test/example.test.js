93b3625a36509b3ad04f0036c9f1d1699d6fe386
Jorge Aguiar Martin <jorge.aguiarmartin@atos.net>
2019-08-30 10:53:20 +0200

    Jest with es6 support

--- test/example.test.js
+++ test/example.test.js
@@ -2,6 +2,14 @@
 import Example from "../src/example";
 
 describe('Example Class Definition', function () {
+
+  let example = new Example();
+
+  it('Should have defined a exampleVariable member', function () {
+    expect(example.exampleVariable).toBeDefined();
+  });
+  it('Should have a exampleVariable getter', function () {
+    expect(example.getExampleVariable()).toEqual(example.exampleVariable);
   it('Should have defined a exampleVariable member', function () {
     let example = new Example();
     expect(example.exampleVariable).toBeDefined();
